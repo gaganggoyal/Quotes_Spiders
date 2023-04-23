134cc2e79983e2a382177301f2ee87e8b4d92d99
gagan goyal <goyalgagan82@gmail.com>
2023-04-23 13:45:25 +0530

    Add yield function inplace of print

--- quotes_spider/spiders/quotes.py
+++ quotes_spider/spiders/quotes.py
@@ -17,11 +17,9 @@ class QuotesSpider(scrapy.Spider):
             author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
             tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
         
-            print ('\n')
-            print (text)
-            print (author)
-            print (tags)
-            print ('\n')
+            yield { 'Text' : text,
+                   'Author' : author, 
+                   'Tags' : tags}
         next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
         absolute_next_page_url = response.urljoin(next_page_url)
         yield scrapy.Request(absolute_next_page_url)
